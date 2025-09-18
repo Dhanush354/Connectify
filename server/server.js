c2f8a6629b18e4ecef6b2806ad7254762ecd7044
Dhanush <dhanushpatnapu@gmail.com>
2025-09-18 12:43:00 +0530

    email remainder added

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { functions, inngest } from './inngest/index.js';
 import { serve } from "inngest/express";
+import { clerkMiddleware } from '@clerk/express'
+import userRouter from './routes/userRoute.js';
 
 const app=express();
 
@@ -12,9 +14,11 @@ await connectDB();
 app.use(express.json());
 app.use(cors());
 
+app.use(clerkMiddleware());
+
 app.get('/',(req,res)=> res.send('Server is running'))
 app.use('/api/inngest', serve({client: inngest, functions}))
-
+app.use('/api/user', userRouter)
 const PORT= process.env.PORT || 4000;
 
 app.listen(PORT,()=> console.log(`Server is running on port ${PORT}`))
